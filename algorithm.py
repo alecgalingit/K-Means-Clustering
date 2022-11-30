8f265671c5746f2b71349b3f564de507f13b91b2
alecgalingit <106795314+alecgalingit@users.noreply.github.com>
2022-11-30 14:59:25 -0500

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,181 @@
+"""
+Primary algorithm for k-Means clustering
+
+This file contains the Algorithm class for performing k-means clustering.
+"""
+import math
+import random
+import numpy
+import dataset
+import cluster
+
+def valid_seeds(value, size):
+    """
+    Returns True if value is a valid list of seeds for clustering.
+
+    A list of seeds is a k-element list OR tuple of integers between 0 and size-1.
+    In addition, no seed element can appear twice.
+
+    Parameter valud: a value to check
+    Precondition: value can be anything
+
+    Paramater size: The database size
+    Precondition: size is an int > 0
+    """
+    assert type(size) == int and size > 0
+
+    if type(value) != list:
+        return False
+    for seed in value:
+        if not isinstance(seed,int):
+            return False
+        if seed < 0 or seed > (size - 1):
+            return False
+        if value.count(seed) > 1:
+            return False
+
+    return True
+
+class Algorithm(object):
+    """
+    A class to manage and run the k-means algorithm.
+
+    The method step() performs one step of the calculation.  The method run() will
+    continue the calculation until it converges (or reaches a maximum number of steps).
+    """
+    # IMMUTABLE ATTRIBUTES (Fixed after initialization with no DIRECT access)
+    # Attribute _dataset: The Dataset for this algorithm
+    # Invariant: _dataset is an instance of Dataset
+    #
+    # Attribute _cluster: The clusters to use at each step
+    # Invariant: _cluster is a non-empty list of Cluster instances
+
+    def getClusters(self):
+        """
+        Returns the list of clusters in this object.
+
+        This method returns the cluster list directly (it does not copy).  Any changes
+        made to this list will modify the set of clusters.
+        """
+        return self._cluster
+
+    def __init__(self, dset, k, seeds=None):
+        """
+        Initializes the algorithm for the dataset ds, using k clusters.
+
+        If the optional argument seeds is supplied, those seeds will be a list OR
+        tuple of indices into the dataset. They specify which points should be the
+        initial cluster centroids. Otherwise, the clusters are initialized by randomly
+        selecting k different points from the database to be the cluster centroids.
+
+        Parameter dset: the dataset
+        Precondition: dset is an instance of Dataset
+
+        Parameter k: the number of clusters
+        Precondition: k is an int, 0 < k <= dset.getSize()
+
+        Paramter seeds: the initial cluster indices (OPTIONAL)
+        Precondition: seeds is None, or a list/tuple of valid seeds.
+        """
+        assert isinstance(dset,dataset.Dataset)
+        assert isinstance(k,int) and k > 0 and k <= dset.getSize()
+        assert seeds == None or valid_seeds(seeds,dset.getSize())
+
+        self._dataset = dset
+        if seeds != None:
+            #assert len(seeds) == k
+            self._cluster = [cluster.Cluster(dset,dset.getPoint(seed)) for seed in seeds]
+        else:
+            random_seeds = [random.sample(range(dset.getSize()),k)][0]
+            self._cluster = [cluster.Cluster(dset,dset.getPoint(seed)) for seed in random_seeds]
+
+
+    def _nearest(self, point):
+        """
+        Returns the cluster nearest to point
+
+        This method uses the distance method of each Cluster to compute the distance
+        between point and the cluster centroid. It returns the Cluster that is closest.
+
+        Ties are broken in favor of clusters occurring earlier in the list returned
+        by getClusters().
+
+        Parameter point: The point to compare.
+        Precondition: point is a tuple of numbers (int or float). Its length is the
+        same as the dataset dimension.
+        """
+        assert dataset.is_point(point) and len(point) == \
+        self._dataset.getDimension()
+
+        distances = {}
+        for cluster in self._cluster:
+            distances[cluster] = cluster.distance(point)
+
+        minimum = min(distances.values())
+        return [key for key in distances if distances[key]==minimum][0]
+
+
+    def _partition(self):
+        """
+        Repartitions the dataset so each point is in exactly one Cluster.
+        """
+        # First, clear each cluster of its points.  Then, for each point in the
+        # dataset, find the nearest cluster and add the point to that cluster.
+        for cluster in self._cluster:
+            cluster.clear()
+        for index in range(len(self._dataset.getContents())):
+            self._nearest(self._dataset.getPoint(index)).addIndex(index)
+
+
+    def _update(self):
+        """
+        Returns True if all centroids are unchanged after an update; False otherwise.
+
+        This method first updates the centroids of all clusters'.  When it is done, it
+        checks whether any of them have changed. It returns False if just one has
+        changed. Otherwise, it returns True.
+        """
+        temp = True
+        for cluster in self._cluster:
+            update = cluster.update()
+            if update == False:
+                temp = False
+
+        return temp
+
+
+    def step(self):
+        """
+        Returns True if the algorithm converges after one step; False otherwise.
+
+        This method performs one cycle of the k-means algorithm. It then checks if
+        the algorithm has converged and returns the appropriate result (True if
+        converged, false otherwise).
+        """
+        self._partition()
+        no_update = self._update()
+
+        return no_update
+
+
+    def run(self, maxstep):
+        """
+        Continues clustering until either it converges or performs maxstep steps.
+
+        After the maxstep call to step, if this calculation did not converge, this
+        method will stop.
+
+        Parameter maxstep: The maximum number of steps to perform
+        Precondition: maxstep is an int >= 0
+        """
+        # Call k_means_step repeatedly, up to maxstep times, until the algorithm
+        # converges.  Stop once you reach maxstep iterations even if the algorithm
+        # has not converged.
+        # You do not need a while loop for this.  Just write a for-loop, and exit
+        # the for-loop (with a return) if you finish early.
+        assert isinstance(maxstep, int) and maxstep >= 0
+
+        for i in range(maxstep):
+            converge = self.step()
+            if converge:
+                return
---
+++ cluster.py
@@ -0,0 +1,203 @@
+"""
+Cluster class for k-Means clustering
+
+This file contains the class cluster, which is the second part of the assignment.  With
+this class done, the visualization can display the centroid of a single cluster.
+"""
+import math
+import random
+import numpy
+import dataset
+
+
+class Cluster(object):
+    """
+    A class representing a cluster, a subset of the points in a dataset.
+
+    A cluster is represented as a list of integers that give the indices in the dataset
+    of the points contained in the cluster.  For instance, a cluster consisting of the
+    points with indices 0, 4, and 5 in the dataset's data array would be represented by
+    the index list [0,4,5].
+
+    A cluster instance also contains a centroid that is used as part of the k-means
+    algorithm.  This centroid is an n-D point (where n is the dimension of the dataset),
+    represented as a list of n numbers, not as an index into the dataset. (This is because
+    the centroid is generally not a point in the dataset, but rather is usually in between
+    the data points.)
+    """
+    # IMMUTABLE ATTRIBUTES (Fixed after initialization with no DIRECT access)
+    # Attribute _dataset: The Dataset for this cluster
+    # Invariant: _dataset is an instance of Dataset
+    #
+    # Attribute _centroid: The centroid of this cluster
+    # Invariant: _centroid is a point (tuple of int/float) whose length is equal to
+    # to the dimension of _dataset.
+    #
+    # MUTABLE ATTRIBUTES (Can be changed at any time, via addIndex, or clear)
+    # Attribute _indices: the indices of this cluster's points in the dataset
+    # Invariant: _indices is a list of ints. For each element ind in _indices,
+    # 0 <= ind < _dataset.getSize()
+
+    # Part A
+    def getIndices(self):
+        """
+        Returns the indices of points in this cluster
+
+        This method returns the indices directly (not a copy). Any changes made to this
+        list will modify the cluster.
+        """
+        return self._indices
+
+    def getCentroid(self):
+        """
+        Returns the centroid of this cluster.
+
+        This getter method is to protect access to the centroid, and prevent someone
+        from changing it accidentally. Because the centroid is a tuple, it is not
+        necessary to copy the centroid before returning it.
+        """
+        return self._centroid
+
+    def __init__(self, dset, centroid):
+        """
+        Initializes a new empty cluster with the given centroid
+
+        Parameter dset: the dataset
+        Precondition: dset is an instance of Dataset
+
+        Parameter centroid: the cluster centroid
+        Precondition: centroid is a tuple of dset.getDimension() numbers
+        """
+        assert isinstance(dset,dataset.Dataset)
+        assert type(centroid) == tuple and len(centroid) == dset.getDimension()
+
+        self._dataset = dset
+        self._centroid = centroid
+        self._indices = []
+
+    def addIndex(self, index):
+        """
+        Adds the given dataset index to this cluster.
+
+        If the index is already in this cluster, this method leaves the
+        cluster unchanged.
+
+        Precondition: index is a valid index into this cluster's dataset.
+        That is, index is an int >= 0, but less than the dataset size.
+        """
+        assert type(index) == int and index >= 0 and index < self._dataset.getSize()
+
+        if index not in self._indices:
+            self._indices.append(index)
+
+
+    def clear(self):
+        """
+        Removes all points from this cluster, but leaves the centroid unchanged.
+        """
+        self._indices.clear()
+
+
+    def getContents(self):
+        """
+        Returns a new list containing copies of the points in this cluster.
+
+        The result is a list of points (tuples of int/float). It has to be computed
+        from the list of indices.
+        """
+        temp = []
+        for i in self.getIndices():
+            temp.append(self._dataset.getPoint(i))
+        return temp
+
+
+    # Part B
+    def distance(self, point):
+        """
+        Returns the euclidean distance from point to this cluster's centroid.
+
+        Parameter point: The point to be measured
+        Precondition: point is a tuple of numbers (int or float), with the same dimension
+        as the centroid.
+        """
+        assert dataset.is_point(point)
+        assert len(point) == self._dataset.getDimension()
+
+        #Creates a nested listed with values of the point and centroid at each
+        #index i
+        all = []
+        for i in range(len(point)):
+            all.append([point[i]])
+        for i in range(len(self.getCentroid())):
+            all[i].append(self.getCentroid()[i])
+        sum = 0
+        for i in range(len(all)):
+            sum += ((all[i][0] - all[i][1])**2)
+
+        return sum**0.5
+
+
+    def getRadius(self):
+        """
+        Returns the maximum distance from any point in this cluster, to the centroid.
+
+        This method loops over the contents of this cluster to find the maximum distance
+        from the centroid.
+        """
+        distances = []
+        for point in self.getContents():
+            distances.append(self.distance(point))
+
+        return max(distances)
+
+    def update(self):
+        """
+        Returns True if the centroid remains the same after recomputation; False otherwise.
+
+        This method recomputes the centroid of this cluster. The new centroid is the
+        average of the of the contents (To average a point, average each coordinate
+        separately).
+
+        Whether the centroid "remained the same" after recomputation is determined by
+        numpy.allclose. The return value should be interpreted as an indication of
+        whether the starting centroid was a "stable" position or not.
+
+        If there are no points in the cluster, the centroid. does not change.
+        """
+        #0 case
+        if len(self.getContents()) == 0:
+            return True
+
+        #Creates an empty nested list containing len(self.getContents()[0]) empty
+        #lists
+        recomputation = []
+        for i in range(len(self.getContents()[0])):
+            recomputation.append([])
+
+        #Adds lists of index of points from self.getContents() to recomputation
+        for point in self.getContents():
+            for i in range(len(point)):
+                recomputation[i].append(point[i])
+
+        #Sets recomputation[i] to mean of recomputation[i], computing the final
+        #recomputation
+        for i in range(len(recomputation)):
+            recomputation[i] = (sum(recomputation[i])/len(recomputation[i]))
+
+        old_centroid = self._centroid
+        self._centroid = recomputation
+
+        return numpy.allclose(old_centroid,recomputation)
+
+
+    def __str__(self):
+        """
+        Returns a String representation of the centroid of this cluster.
+        """
+        return str(self._centroid)+':'+str(self._indices)
+
+    def __repr__(self):
+        """
+        Returns an unambiguous representation of this cluster.
+        """
+        return str(self.__class__) + str(self)
